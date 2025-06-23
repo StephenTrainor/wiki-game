3a427822f1825144a27dfeec201372d8836dde47
StephenTrainor <stephontrainor@gmail.com>
2025-06-23 17:40:50 +0000

    Completed relevant functions for Exercise 1

--- src/lambda_soup_utilities.ml
+++ src/lambda_soup_utilities.ml
@@ -34,7 +34,8 @@ let get_list_items contents : string list =
   parse contents
   $$ "li"
   |> to_list
-  |> List.map ~f:(fun li -> texts li |> String.concat ~sep:"" |> String.strip)
+  |> List.map ~f:(fun li ->
+    texts li |> String.concat ~sep:"" |> String.strip)
 ;;
 
 let%expect_test "get_list_items" =
@@ -60,20 +61,40 @@ let%expect_test "get_list_items" =
 
 (* Gets the first item of all unordered lists contained in an HTML page. *)
 let get_first_item_of_all_unordered_lists contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  parse contents
+  $$ "ul"
+  |> to_list
+  |> List.map ~f:(fun ul ->
+    select "li" ul
+    |> to_list
+    |> List.hd_exn
+    |> texts
+    |> String.concat ~sep:"")
 ;;
 
 (* Gets the first item of the second unordered list in an HTML page. *)
 let get_first_item_of_second_unordered_list contents : string =
-  ignore (contents : string);
-  failwith "TODO"
+  let second_of_list (l : 'a list) : 'a = List.nth_exn l 1 in
+  let open Soup in
+  parse contents
+  $$ "ul"
+  |> to_list
+  |> second_of_list
+  |> select "li"
+  |> to_list
+  |> List.hd_exn
+  |> texts
+  |> String.concat ~sep:""
 ;;
 
 (* Gets all bolded text from an HTML page. *)
 let get_bolded_text contents : string list =
-  ignore (contents : string);
-  failwith "TODO"
+  let open Soup in
+  parse contents
+  $$ "b"
+  |> to_list
+  |> List.map ~f:(fun b_tag -> texts b_tag |> String.concat ~sep:"")
 ;;
 
 (* [make_command ~summary ~f] is a helper function that builds a simple HTML parsing
@@ -92,12 +113,15 @@ let make_command ~summary ~f =
 ;;
 
 let print_title_command =
-  make_command ~summary:"print the title from an HTML page" ~f:(fun contents ->
-    [ get_title contents ])
+  make_command
+    ~summary:"print the title from an HTML page"
+    ~f:(fun contents -> [ get_title contents ])
 ;;
 
 let print_list_items_command =
-  make_command ~summary:"print all list items from an HTML page" ~f:get_list_items
+  make_command
+    ~summary:"print all list items from an HTML page"
+    ~f:get_list_items
 ;;
 
 let print_first_item_of_all_unordered_lists_command =
@@ -113,7 +137,9 @@ let print_first_item_of_second_unordered_list_command =
 ;;
 
 let print_bolded_text_command =
-  make_command ~summary:"print all bolded text in an HTML page" ~f:get_bolded_text
+  make_command
+    ~summary:"print all bolded text in an HTML page"
+    ~f:get_bolded_text
 ;;
 
 let command =
