c4f2ed5caa9b39d84cff85ca41444480c9fd65e7
StephenTrainor <stephontrainor@gmail.com>
2025-06-30 15:01:04 +0000

    Wrote scaffolding for min heap

---
+++ src/min_heap.ml
@@ -0,0 +1,4 @@
+(* module Min_heap = struct
+   type ('a, 'b) t = 'a Dynamic_array.t
+
+   end *)
---
+++ src/min_heap.mli
@@ -0,0 +1,7 @@
+(* module Min_heap : sig
+  type ('a, 'b) t
+
+  val peek : ('a, 'b) t -> 'b
+  val remove_min : ('a, 'b) t -> 'b
+  val add : ('a, 'b) t -> 'a -> 'b -> unit *)
+(* end *)
